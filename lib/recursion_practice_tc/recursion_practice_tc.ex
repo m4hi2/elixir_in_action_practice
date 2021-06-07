43a72221628fb477df65a3731a18ed27b31afdfb
Mahir Labib Chowdhury <optical.mahir@gmail.com>
2021-06-07 10:13:23 +0600

    feat: formatting and specs added

--- lib/recursion_practice_tc/recursion_practice_tc.ex
+++ lib/recursion_practice_tc/recursion_practice_tc.ex
@@ -1,24 +1,29 @@
 defmodule ElixirInAction.RecursionPracticeTC do
-
+  @spec list_len(list) :: non_neg_integer
   def list_len(list) do
     do_list_len(list)
   end
+
   defp do_list_len(list, n \\ 0)
   defp do_list_len([], n), do: n
+
   defp do_list_len(list, n) do
     [_head | tail] = list
     n = n + 1
     do_list_len(tail, n)
   end
 
-    def range(n, m) do
-      do_range(n, m)
-      |> Enum.reverse()
-    end
-    defp do_range(n, m, list \\ [])
-    defp do_range(n, n, list), do: [n | list]
-    defp do_range(n, m, list) do
-      list = [n | list]
-      do_range(n + 1, m, list)
-    end
+  @spec range(integer, integer) :: list
+  def range(n, m) do
+    do_range(n, m)
+    |> Enum.reverse()
   end
+
+  defp do_range(n, m, list \\ [])
+  defp do_range(n, n, list), do: [n | list]
+
+  defp do_range(n, m, list) do
+    list = [n | list]
+    do_range(n + 1, m, list)
+  end
+end
